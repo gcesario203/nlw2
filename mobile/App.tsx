a75eeac3e9882ff7b93a06e65c69f929abe17ed0
Gabriel Cesario <cesario203@outlook.com>
2020-08-08 22:28:29 -0300

    Commit atrasado com o mobile no meio do caminho kkkkkkkkk

---
+++ mobile/App.tsx
@@ -0,0 +1,31 @@
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import Landing from './src/pages/Landing'
+import { AppLoading } from 'expo'
+import {
+  useFonts,
+  Archivo_700Bold,
+  Archivo_400Regular
+} from '@expo-google-fonts/archivo'
+import { Poppins_400Regular, Poppins_600SemiBold } from '@expo-google-fonts/poppins'
+
+
+export default function App() {
+  let [fontsLoaded] = useFonts({
+    Archivo_700Bold,
+    Archivo_400Regular,
+    Poppins_400Regular,
+    Poppins_600SemiBold
+  })
+
+  if (!fontsLoaded) {
+    return <AppLoading></AppLoading>
+  } else {
+    return (
+      <>
+        <Landing />
+        <StatusBar style="light"></StatusBar>
+      </>
+    );
+  }
+}
---
+++ mobile/src/pages/Landing/styles.ts
@@ -0,0 +1,62 @@
+import {StyleSheet} from 'react-native'
+
+const styles = StyleSheet.create({
+    container:{
+        flex:1,
+        backgroundColor:'#8257E5',
+        justifyContent:"center",
+        padding:40
+    },
+    banner:{
+        width:'100%',
+        resizeMode:'contain'
+    },
+    title:{
+        color:'#fff',
+        fontSize:20,
+        lineHeight:30,
+        marginTop:80
+    },
+
+    buttonsContainer:{
+        flexDirection:'row',
+        marginTop:40,
+        justifyContent:'space-between'
+    },
+
+    button:{
+        height:150,
+        width:'48%',
+        backgroundColor:'#333',
+        borderRadius:8,
+        padding:24,
+        justifyContent:'space-between',
+        alignItems:'center'
+    },
+
+    buttonPrimary:{
+        backgroundColor:'#9871f5'
+    },
+
+    buttonSecondary:{
+        backgroundColor:'#04d361'
+    },
+
+    buttonText:{
+        color:'#fff',
+        fontSize:20,
+        fontFamily:'Archivo_700Bold',
+        width:'100%',
+    },
+
+    totalConnections:{
+        fontFamily:'Poppins_400Regular',
+        color:'#f3c2ff',
+        fontSize:12,
+        lineHeight:20,
+        maxWidth:140,
+        marginTop:40
+    }
+})
+
+export default styles
